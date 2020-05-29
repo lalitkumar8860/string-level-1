216f714fcb55a2ae30c344ddd8cfba3b254d6586
Lalit kumar <lalitkumar@.github.com>
2020-05-29 13:03:30 +0530

    1.	WAP to accept a String from User and Display it Back on Screen

---
+++ src/level/pkg1/Level1.java
@@ -0,0 +1,20 @@
+
+package level.pkg1;
+
+import java.util.Scanner;
+
+
+public class Level1 {
+
+   
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("Enter the string");
+        name = sc.next();
+        System.out.println("string is..." + name);
+        
+    }
+    
+}
